f5a1cdf3dc4584db7096bc2539e7f899f21b7931
Aleksandr Kochkurkin <89263693878@mail.ru>
2024-12-02 09:27:00 +0300

    commit_02 02_12_2024

--- src/main/java/ru/kata/spring/boot_security/demo/servises/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/servises/UserServiceImpl.java
@@ -2,7 +2,6 @@ package ru.kata.spring.boot_security.demo.servises;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
